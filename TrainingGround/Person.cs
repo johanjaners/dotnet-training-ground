613d7736ff3ec1d5a829f82c6086f01dc57f71b1
Johan Janérs <johanjaners@gmail.com>
2025-09-06 21:00:57 +0200

    Person.cs: Name field private,  GetName and SetName method added

--- TrainingGround/Person.cs
+++ TrainingGround/Person.cs
@@ -1,12 +1,35 @@
 public class Person
 {
-    public string Name;
+
+
+    private string? Name;
+    public string GetName
+    {
+        return this.Name;
+    }
+
+
+
+
+    public string SetName(string nameToSet)
+    {
+        if (nameToSet.Length > 5)
+        {
+            this.Name = nameToSet
+        }
+    }
+
+
+
     public int birthYear;
     public double LengthInMeters;
+
+
 
     public Person() { }
     public Person(string name)
     {
         this.Name = name;
     }
+    
 }
