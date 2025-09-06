a25564c4f7e210599f0d82a802b2cef2a11eace5
Johan Janérs <johanjaners@gmail.com>
2025-09-06 19:14:52 +0200

    refactor(tests): introduce private AgeCalculator field in test calss constructor to remove duplication

--- TrainingGround.Tests/AgeCalculatorTests.cs
+++ TrainingGround.Tests/AgeCalculatorTests.cs
@@ -2,18 +2,28 @@
 
 public class AgeCalculatorTests
 {
+    private AgeCalculator _calculator;
+
+    public AgeCalculatorTests()
+    {
+        _calculator = new AgeCalculator();
+    }
+
     [Fact]
     public void Someone_born_1972_is_50_in_2022()
     {
-        
-        // arrange
-        var calculator = new AgeCalculator();
-
         //act
-        var age = calculator.GetAge(2022, 2022);
+        var age = _calculator.GetAge(1972, 2022);
+        //assert
+        Assert.Equal(50, age);   
+    }
 
+    [Fact]
+    public void Someone_born_2022_is_0_in_2022()
+    {
+        //act
+        var age = _calculator.GetAge(2022, 2022);
         //assert
         Assert.Equal(0, age);
-        
     }
 }
--- TrainingGround/obj/Debug/net9.0/TrainingGround.AssemblyInfo.cs
+++ TrainingGround/obj/Debug/net9.0/TrainingGround.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingGround")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab5955d116688401dcbbbe2ca49ea0cb23fe2edf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a9f2ad7b228a053d1fada851c47da185a65e74")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingGround")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingGround")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
