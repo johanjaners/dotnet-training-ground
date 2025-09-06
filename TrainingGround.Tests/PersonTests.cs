b3e39ec492c409340fd5d0286b1028fcfa1411a3
Johan Janérs <johanjaners@gmail.com>
2025-09-06 20:03:51 +0200

    PersonTests.cs updated using contructor

--- TrainingGround.Tests/PersonTests.cs
+++ TrainingGround.Tests/PersonTests.cs
@@ -4,9 +4,10 @@ public class PersonTests
     public void Can_create_person_using_parameterless_constructor()
     {
         // act
-        var p = new Person();
+        var p = new Person("Marcus");
 
         // assert
         Assert.NotNull(p);
+        Assert.Equal("Marcus", p.Name);
     }
 }
