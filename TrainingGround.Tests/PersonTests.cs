f4dd1cbde37819d42ec40e8fd1b473f878e28f8a
Johan Janérs <johanjaners@gmail.com>
2025-09-07 11:51:23 +0200

    (tests):PersonTests.cs: A_person_born_1992_is_30_2022 added

--- TrainingGround.Tests/PersonTests.cs
+++ TrainingGround.Tests/PersonTests.cs
@@ -24,7 +24,7 @@ public class PersonTests
         // assert
         Assert.Equal(50, age);
     }
- 
+
     [Fact]
     public void A_person_born_1982_is_40_2022()
     {
@@ -36,6 +36,19 @@ public class PersonTests
 
         // assert
         Assert.Equal(40, age);
+    }
+
+    [Fact]
+    public void A_person_born_1992_is_30_2022()
+    {
+        // arrange
+        var p = new Person(1992);
+
+        // act
+        var age = p.GetAge(2022);
+
+        // assert
+        Assert.Equal(30, age);
     }
 
 }
--- TrainingGround.Tests/obj/Debug/net9.0/TrainingGround.Tests.AssemblyInfo.cs
+++ TrainingGround.Tests/obj/Debug/net9.0/TrainingGround.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingGround.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6143b485baf60737234d30b3ce472fd1bfd7a8b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92915d83a8499e8e1f2578884fb6608822bab5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingGround.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingGround.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
