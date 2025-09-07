a567b3626d8119dbe64303b37ab46f2ece0dc272
Johan Janérs <johanjaners@gmail.com>
2025-09-07 18:45:49 +0200

    (test) TypesTests.cs: reference_types , value_types test  added, , Person.cs: Properties  added to LengthInMeters, get , set

--- TrainingGround.Tests/TypesTests.cs
+++ TrainingGround.Tests/TypesTests.cs
@@ -19,7 +19,21 @@ public class TypesTests
         Assert.Equal(1.96, a.LengthInMeters);
     }
 
+    [Fact]
+    public void value_types_cannot_be_changed_via_reference()
+    {
+        // arrange
+        double aLengthInMeters = 1.95;
+        double bLengthInMeters = aLengthInMeters;
 
+        // act
+        bLengthInMeters = 1.96;
+
+        // assert
+        Assert.Equal(1.96, bLengthInMeters);
+        Assert.Equal(1.95, aLengthInMeters);
+
+    }
 }
 
 
