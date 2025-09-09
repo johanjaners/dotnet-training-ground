e988586698d67535de26f1bf59c1eec97336f864
Johan Janérs <johanjaners@gmail.com>
2025-09-09 16:22:35 +0200

    added  Address.cs, (test) PersonTests.cs: added a_person_has_an_adress, Person.cs: added property address

--- TrainingGround.Tests/PersonTests.cs
+++ TrainingGround.Tests/PersonTests.cs
@@ -1,3 +1,6 @@
+using System.Net.Sockets;
+using TrainingGround;
+
 public class PersonTests
 {
     [Fact]
@@ -29,6 +32,52 @@ public class PersonTests
         Assert.Equal(expectedAge, age);
     }
 
+    [Fact]
+    public void an_employee_is_a_person()
+    {
+        // act
+        var emp = new Employee();
+        emp.LengthInMeters = 1.95;
+
+        // assert
+        Assert.IsType(typeof(Employee), emp);
+        Assert.Equal(1.95, emp.LengthInMeters);
+    }
+
+
+    [Fact]
+    public void an_employee_has_an_employeeId()
+    {
+        // act
+        var emp = new Employee("Marcus", "234-BDAS");
+
+
+        // assert
+        Assert.IsType(typeof(Employee), emp);
+        Assert.Equal("Marcus", emp.Name);
+        Assert.Equal("234-BDAS", emp.EmployeeId);
+
+    }
+
+    [Fact]
+    public void a_person_has_an_adress()
+    {
+        // arrange
+        var p = new Person("Marcus");
+
+        // act
+        p.Address = new Address();
+        p.Address.Street = "A Street";
+        p.Address.StreetNo = 23;
+        p.Address.City = "Stockholm";
+
+        // assert
+        Assert.NotNull(p.Address);
+        Assert.IsType(typeof(Address), p.Address);
+
+
+
+    }
 
     /*  [Fact] age tests
 
@@ -72,4 +121,11 @@ public class PersonTests
         }
     */
 
+}
+
+internal class Address
+{
+    public Address()
+    {
+    }
 }
