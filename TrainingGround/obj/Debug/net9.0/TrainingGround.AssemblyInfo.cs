a0f089090826e30991d21c34365a95deef872de6
Johan Janérs <johanjaners@gmail.com>
2025-09-12 14:17:17 +0200

    ConditionalTests.cs: ShoudlReturnUnder18_WhenKid test added

--- TrainingGround/obj/Debug/net9.0/TrainingGround.AssemblyInfo.cs
+++ TrainingGround/obj/Debug/net9.0/TrainingGround.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingGround")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57e585b42702e5fde0b29d06564eabe59f6c45b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b9a73cb49319919527ac6e43599f604012d87f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingGround")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingGround")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
