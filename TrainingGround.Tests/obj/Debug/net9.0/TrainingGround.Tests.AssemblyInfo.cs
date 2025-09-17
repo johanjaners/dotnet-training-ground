c7512d92215435f7553a76347640e73fdf2936e4
Johan Janérs <johanjaners@gmail.com>
2025-09-17 21:52:12 +0200

    Calculator.cs: added Calcdiff method

--- TrainingGround.Tests/obj/Debug/net9.0/TrainingGround.Tests.AssemblyInfo.cs
+++ TrainingGround.Tests/obj/Debug/net9.0/TrainingGround.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingGround.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c25f77e74f2a23e9adc58950815abd758f08d84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63f6cdd2f93c412416965fb5d80feabfa44a280")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingGround.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingGround.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
